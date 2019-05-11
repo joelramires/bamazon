5074aacea8ba6bf9ba17d64b41bde1705cee30ba
Joel Ramirez <joelramirez@MacBook-Pro-5.local>
2019-05-11 14:30:28 -0400

    update

--- bamazon.sql
+++ bamazon.sql
@@ -20,20 +20,25 @@ VALUES ("Street Fighter 5","ENTERTAINMENT",34.99,150),
     ("StarKist Chunk Light Tuna Pack of 8","GROCERY",6.50,300),
     ("Bulova Men's 43mm Classic Automatic Watch","CLOTHING",299.98,5),
     ("Levi's Mens 510 Skinny-fit Jean","CLOTHING",29.99,35),
-    ("Perfect Fitness Ab Carver Pro Roller","SPORTS & OUTDOORS",31.49,42),
+    ("Survival Towel","SPORTS & OUTDOORS",39.99,42),
     ("The Matrix","ENTERTAINMENT",11.99,25),
     ("Mad Max: Fury Road","ENTERTAINMENT",25.50,60),
     ("Catan","ENTERTAINMENT",30.50,35),
-    ("Playstation 4","ELECTRONICS",299.99,20);
+    ("Playstation 4","ENTERTAINMENT",299.99,20);
 
 CREATE TABLE Departments(
     DepartmentID MEDIUMINT AUTO_INCREMENT NOT NULL,
     DepartmentName VARCHAR(50) NOT NULL,
+    OverHeadCosts DECIMAL(10,2) NOT NULL,
+    TotalSales DECIMAL(10,2) NOT NULL,
     PRIMARY KEY(DepartmentID));
 
-    INSERT INTO Departments(DepartmentName)
-VALUES ('ENTERTAINMENT',),
-    ('ELECTRONICS'),
-    ('GROCERY'),
-    ('CLOTHING'),
-    ('SPORTS & OUTDOORS');
+    INSERT INTO Departments(DepartmentName, OverHeadCosts, TotalSales)
+VALUES ('ENTERTAINMENT', 50000.00, 15000.00),
+    ('ELECTRONICS', 20000.00, 12000.00),
+    ('HOME', 30000.00, 15000.00),
+    ('BODY & HEALTH', 3000.00, 12000.00),
+    ('GROCERY', 1200.00, 15000.00),
+    ('KIDS', 40000.00, 12000.00),
+    ('CLOTHING', 35000.00, 15000.00),
+    ('SPORTS & OUTDOORS', 12000.00, 12000.00);
